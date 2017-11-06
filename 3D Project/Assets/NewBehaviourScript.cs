8fe32ed550ceeeb36705567b3d32e43217e23226
Beverly Li <bel058@UCSD.EDU>
2017-11-05 18:46:57 -0800

    test

---
+++ 3D Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 vec = new Vector3(Mathf.Clamp(Input.mousePosition.x/4, -4.0f, 4.0f), 
+			Mathf.Clamp(Input.mousePosition.y/4, 3.5f, 8.5f), 0.0f);
+
+		transform.position = vec;
+	}
+}
---
+++ 3D Project/Assets/SphereMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 vec = new Vector3(Mathf.Clamp(transform.position.x, -4.0f, 4.0f), 
+			Mathf.Clamp(transform.position.y, 3.5f, 8.5f), 0.0f);
+	
+	}
+		
+}
