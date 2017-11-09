b86e483b89aa90f5997602a58e662e1c49f1cd32
Beverly Li <bel058@UCSD.EDU>
2017-11-08 20:20:30 -0800

    vrproject

---
+++ VR Project/Assets/Moving.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Moving : MonoBehaviour {
+
+	public float velocity;
+	public Button myButton;
+	public Vector3 startPlayerPos;
+
+	// Use this for initialization
+	void Start () {
+		velocity = 0;
+		myButton btn = myButton.GetComponent<myButton>();
+		btn.onClick.AddListener(TaskOnClick);
+/*		player = GameObject.Find ("player");
+		Transform playerTransform = player.transform;
+		Vector3 startPlayerPos = playerTransform.position;
+		startPlayerPos = player.position;*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		velocity -= .01;
+		transform.z = transform.z + velocity;
+
+	}
+
+	void TaskOnClick() {
+		velocity += 3; 
+	}
+}
